f1f31c6a1ae33611fc3a9eefc9a79fbf4c5bceb0
Jh0n16 <57267877+Jh0n16@users.noreply.github.com>
2024-04-20 20:55:43 -0300

    fix: Corrigindo rotas da API
    Essa alteração adiciona todos os arquivos necessarios para o funcionamento adequado das rotas de API

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\ClienteController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::get('cliente/', [ClienteController::class, 'index'])->name('cliente.index');
 Route::post('cliente/', [ClienteController::class, 'store'])->name('cliente.store');
 Route::get('cliente/{cliente}', [ClienteController::class, 'show'])->name('cliente.show');
 Route::put('cliente/{cliente}', [ClienteController::class, 'update'])->name('cliente.update');
 Route::delete('cliente/{cliente}', [ClienteController::class, 'destroy'])->name('cliente.destroy');
-
