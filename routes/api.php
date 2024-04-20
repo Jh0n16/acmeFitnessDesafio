b6b6a67ed90f27362abebd4201c38fbebd08baaa
Jh0n16 <57267877+Jh0n16@users.noreply.github.com>
2024-04-20 19:42:34 -0300

    feat: Adicionando as rotas na API para Clientes
    Essa alteração define as rotas utilizadas para nossa API juntamente com um nome para elas

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('cliente/', [ClienteController::class, 'index'])->name('cliente.index');
+Route::post('cliente/', [ClienteController::class, 'store'])->name('cliente.store');
+Route::get('cliente/{cliente}', [ClienteController::class, 'show'])->name('cliente.show');
+Route::put('cliente/{cliente}', [ClienteController::class, 'update'])->name('cliente.update');
+Route::delete('cliente/{cliente}', [ClienteController::class, 'destroy'])->name('cliente.destroy');
+
