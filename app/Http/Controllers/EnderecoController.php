c89ebae6620e0f7328c456debbbff910c89d4e29
Jh0n16 <57267877+Jh0n16@users.noreply.github.com>
2024-04-21 16:28:08 -0300

    feat(Adicionando rotas) Adicioando novas rotas para Endereco
    Essa alteração cria novas rotas a API para serem utilizadas para lidar com endereços

---
+++ app/Http/Controllers/EnderecoController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EnderecoRequest;
+use App\Models\Endereco;
+use Illuminate\Http\Request;
+
+class EnderecoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Endereco::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(EnderecoRequest $request)
+    {
+        return Endereco::create([
+            'logradouro' => $request->input('logradouro'),
+            'cidade' => $request->input('cidade'),
+            'bairro' => $request->input('bairro'),
+            'numero' => $request->input('numero'),
+            'cep' => $request->input('cep'),
+            'complemento' => $request->input('complemento')
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Endereco::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(EnderecoRequest $request, string $id)
+    {
+        return Endereco::findOrFail($id)->update([
+            'logradouro' => $request->input('logradouro'),
+            'cidade' => $request->input('cidade'),
+            'bairro' => $request->input('bairro'),
+            'numero' => $request->input('numero'),
+            'cep' => $request->input('cep'),
+            'complemento' => $request->input('complemento')
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return Endereco::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\EnderecoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,8 +9,16 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Clientes
 Route::get('cliente/', [ClienteController::class, 'index'])->name('cliente.index');
 Route::post('cliente/', [ClienteController::class, 'store'])->name('cliente.store');
 Route::get('cliente/{cliente}', [ClienteController::class, 'show'])->name('cliente.show');
 Route::put('cliente/{cliente}', [ClienteController::class, 'update'])->name('cliente.update');
 Route::delete('cliente/{cliente}', [ClienteController::class, 'destroy'])->name('cliente.destroy');
+
+// Enderecos
+Route::get('endereco/', [EnderecoController::class, 'index'])->name('endereco.index');
+Route::post('endereco/', [EnderecoController::class, 'store'])->name('endereco.store');
+Route::get('endereco/{endereco}', [EnderecoController::class, 'show'])->name('endereco.show');
+Route::put('endereco/{endereco}', [EnderecoController::class, 'update'])->name('endereco.update');
+Route::delete('endereco/{endereco}', [EnderecoController::class, 'destroy'])->name('endereco.destroy');
