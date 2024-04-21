8ed7ca817655872a19e8c2ddd06346f242e106bf
Jh0n16 <57267877+Jh0n16@users.noreply.github.com>
2024-04-21 19:21:56 -0300

    fix: Concertando a model dos produtos
    Essa alteração muda o código da model Produto criando o atributo fillable e removendo todos os outros que eram desnecessários

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -9,15 +9,17 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Produto extends Model
 {
     use HasFactory;
-    protected $table = "produtos";
-    protected $primaryKey = "id";
-    protected $nome;
-    protected $cor;
-    protected $imagem;
-    protected $preco;
-    protected $descricao;
-    protected $dataDeCadastro;
-    protected $peso;
+    protected $table = 'produtos';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'nome',
+        'cor',
+        'imagem',
+        'preco',
+        'descricao',
+        'peso',
+    ];
+
     protected function categoria(): BelongsTo
     {
         return $this->belongsTo(Categoria::class);
